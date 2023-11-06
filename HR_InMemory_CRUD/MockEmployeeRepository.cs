67a80be52bbb69314a537f0cf65ccb886bc7880c
ShubhamMore04 <148211719+ShubhamMore04@users.noreply.github.com>
2023-11-06 20:21:33 +0530

    HR_In_Memory_CRUD

---
+++ HR_InMemory_CRUD/MockEmployeeRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_InMemory_CRUD
+{
+    internal class MockEmployeeRepository : IEmployeeRepository
+    {
+        private static List<Employee> _employeeList;
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "ShubhamM", Salary = 80000, Gender = "Male", Address = "Thane" },
+                new Employee() { Id = 2, Name = "Akshay", Salary = 78000, Gender = "Male", Address = "Kolhapur" },
+                new Employee() { Id = 3, Name = "ShubhamG", Salary = 77000, Gender = "Male", Address = "Solapur" },
+                new Employee() { Id = 4, Name = "Ajinkya", Salary = 82000, Gender = "Male", Address = "C.SambhajiNagar"},
+                new Employee() { Id = 5, Name = "Yash", Salary = 77000, Gender = "Male", Address = "Nagpur"},
+                new Employee() { Id = 6, Name = "Sushil", Salary = 83000, Gender = "Male", Address = "C.SambhajiNagar"},
+            };
+        }
+        public Employee Add(Employee employee)
+        {
+            employee.Id = _employeeList.Max(e => e.Id)+1;
+            _employeeList.Add(employee);
+            return employee;
+
+        }
+
+        public Employee Delete(int Id)
+        {
+            Employee employee = _employeeList.FirstOrDefault(e => e.Id == Id);
+            if (employee != null) 
+            {
+                _employeeList.Remove(employee);
+            }
+            return employee;
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            return _employeeList;
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+
+        public Employee Update(Employee employeeChanges)
+        {
+            Employee employee = _employeeList.FirstOrDefault(e => e.Id == employeeChanges.Id);
+            if (employee != null) 
+            {
+                employee.Name = employeeChanges.Name;
+                employee.Salary = employeeChanges.Salary;
+                employee.Gender = employeeChanges.Gender;
+                employee.Address = employeeChanges.Address;
+            }
+            return employee;
+        }
+    }
+}
---
+++ HR_InMemory_CRUD/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace HR_InMemory_CRUD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MockEmployeeRepository db = new MockEmployeeRepository();
+            Console.WriteLine("SHOW EMPLOYEE LIST --->");
+            Display(db);
+
+            Console.WriteLine("ADD EMPLOYEE --->");
+            Employee a = db.Add (new Employee {Name = "Amar", Salary = 84000, Gender = "Male", Address = "Nanded" });
+            Console.WriteLine("Record Added: {0} {1} {2} {3} {4}", a.Id, a.Name, a.Salary, a.Gender, a.Address);
+            Display(db);
+
+            Console.WriteLine("GET EMPLOYEE WITH ID--->");
+            Employee e= db.GetEmployee(2);
+            Console.WriteLine("{0} {1} {2} {3} {4}", e.Id, e.Name, e.Salary, e.Gender, e.Address);
+            Console.WriteLine("------------------------------------------------");
+
+            Console.WriteLine("DELETE EMPLOYEE --->");
+            db.Delete(5);
+            Display(db);
+
+            Console.WriteLine("UPDATE EMPLOYEE --->");
+            Employee m = new Employee {Id = 4, Name = "Rohit", Salary = 76000, Gender = "Male", Address = "Taj" };
+            UpdateData(m, db);
+        }
+
+        static void Display(MockEmployeeRepository db) 
+        {
+            foreach(Employee a in db.GetAllEmployee())
+                Console.WriteLine("{0} {1} {2} {3} {4}", a.Id, a.Name, a.Salary, a.Gender, a.Address);
+
+            Console.WriteLine("------------------------------------------------");
+        }
+
+        static void UpdateData(Employee e, MockEmployeeRepository db)
+        {
+            db.Update(e);
+            Display(db);
+        }
+    }
+}
